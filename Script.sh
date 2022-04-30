32c59f8e0a99682910a2f9ed21a784265ef52750
Devin Moreland <devinmoreland20@gmail.com>
2022-04-30 02:27:38 +0000

    created a new script with the dev/null

---
+++ Script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#This is a script to add a new user to our server with a defualt home directory. 
+
+echo -n "Enter new username:"  
+read name
+
+getent passwd $name
+ 
+if [ $? -eq 0 ]; then
+	echo "User exist."
+	echo "Exiting... Please try again with new username."
+
+else
+	echo "Creating new user..."
+sleep 1
+sudo useradd -m $name
+	echo "Created user $name!"
+fi
+
+#Goal is to make this allow me to keep entering username without having to rerun the script, however I will attempt to fix this once I have a better understanding of vim.. 
