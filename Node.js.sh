a6ef9241d0d226a19ac5dbec52b6235412e546a6
devinmoreland20 <77422898+devinmoreland20@users.noreply.github.com>
2022-05-06 18:35:31 -0400

    Create Node.js.sh

---
+++ Node.js.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum install curl
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+run node to confirm node is installed. 
+npx create-react-app amplifyapp
+npx create-react-app amplifyapp
+cd amplifyapp
+npm start
